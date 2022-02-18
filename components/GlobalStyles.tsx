e127b5f8ff45fa66be001ac6a3f4c92446ed0f19
Unbound <f1arenova1@gmail.com>
2022-02-18 14:57:01 +0300

    init with typescript and styled components

---
+++ components/GlobalStyles.tsx
@@ -0,0 +1,81 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  * {
+    margin: 0;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  html,
+  body,
+  #root {
+    height: 100%;
+  }
+
+  body {
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  img,
+  picture,
+  video,
+  canvas,
+  svg {
+    display: block;
+    max-width: 100%;
+  }
+
+  /* Remove built-in form typography styles */
+  input,
+  button,
+  textarea,
+  select {
+    font: inherit;
+  }
+
+  /* Avoid text overflows */
+  p,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    overflow-wrap: break-word;
+  }
+
+  /* Create a stacking context, without a z-index.
+    This ensures that all portal content (modals and tooltips) will float above the app.
+  */
+  #root,
+  #__next {
+    isolation: isolate;
+  }
+
+  /* GLOBAL STYLES */
+  *,
+  *:before,
+  *:after {
+    font-family: "Noto Sans", sans-serif;
+  }
+
+  html {
+    --primary-blue: hsla(203, 66%, 56%);
+    --secondary-blue: hsl(204, 87%, 81%, 0.5);
+    // silence the warning
+    --reach-dialog: 1;
+  }
+`;
+
+export default GlobalStyles;
