2db95fba3f552d190e50f69c10aa58a03b2d37c2
Unbound <f1arenova1@gmail.com>
2022-02-26 18:48:23 +0300

    upd favcion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,19 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="description"
           content="Vlad Moiseenko is a frontend developer based in Moscow. Currently focused on building fascinating web projects and learning new things."
         />
+
+        {/*  old icon format */}
+        <link rel="icon" sizes="any" href="favicon.ico" />
+        {/* svg icon */}
+        <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+        {/* apple icon */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="https://wikispeedrun.org/apple-touch-icon.png"
+        />
+
+        <link rel="manifest" href="https://wikispeedrun.org/site.webmanifest" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
