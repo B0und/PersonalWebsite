d516c6cce5bccfb4a5923614902e3404e6f449aa
Unbound <unbound9dev@gmail.com>
2022-06-26 00:10:45 +0300

    update projects

--- helpers/data.ts
+++ helpers/data.ts
@@ -9,20 +9,36 @@ export const libraries = [
   "React",
   "Styled components",
   "Next.js",
-  "Jest",
-  "React-router",
+  "Jest + RTL",
+  "Playwright",
   "...rest",
 ];
 
 export const mainProject = {
-  title: "Wikipedia Speedrun Game",
-  text: "The goal of the game is to navigate from a starting wikipedia article to another one, in the least amount of clicks and time.",
-  libraries: ["React", "Redux", "React-router", "Create React App", "Emotion", "Reach UI, Mantine"],
-  github: "https://github.com/B0und/WikiSpeedrun",
-  live: "https://wikispeedrun.org/",
+  title: "Circle Forms",
+  text: "This is a google forms inspired project adopted for an integration with a rythm game called osu!. The most challenging part of this application is managing complex dynamic state of multiple question types, with arbitrary number of options. You can check out some of the functionality on github (since the production app requires authentication)",
+  libraries: [
+    "React",
+    "Next.js",
+    "React Hook Form",
+    "Tailwind",
+    "C# backend",
+    "React Testing Library",
+    "Playwright",
+    "Reach UI, Mantine",
+  ],
+  github: "https://github.com/B0und/circleforms",
+  live: "https://circleforms.net/",
 };
 
 export const projects = [
+  {
+    title: "Wikipedia Speedrun Game",
+    description:
+      "The goal of the game is to navigate from a starting wikipedia article to another one, in the least amount of clicks and time. Made with CRA, Redux, React-router, Emotion, Reach UI, Mantine.",
+    github: "https://github.com/B0und/WikiSpeedrun",
+    live: "https://wikispeedrun.org",
+  },
   {
     title: "Nier loading animation",
     description: "A loading animation from Nier Reincarnation replicated with GSAP.",
@@ -31,12 +47,6 @@ export const projects = [
   },
   {
     title: "Secret Project #001",
-    description: "Currently in development!",
-    github: "#",
-    live: "#",
-  },
-  {
-    title: "Secret Project #002",
     description: "Currently in development!",
     github: "#",
     live: "#",
